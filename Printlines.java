33f29a5a79ef50121c43bdd3ca5b981909254d80
Hazen <Hazen2@users.noreply.github.com>
2014-10-16 19:18:24 -1000

    Update Printlines.java
    Added escape sequence.

--- Printlines.java
+++ Printlines.java
@@ -6,5 +6,8 @@ public class Printlines
     System.out.println("println will advance the cursor to the next line after output");
     System.out.print("However, print will ");
     System.out.print("not advance to the next line after output.");
+    
+    // Discuss escape sequence /n
+    System.out.print("You may also use an escape sequence like so: \nApples \nGrapes \nTurtles");
   }
 }
