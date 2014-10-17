c4bb59a44f7cdf109691a9467534631210e42045
Hazen <Hazen2@users.noreply.github.com>
2014-10-16 18:55:24 -1000

    Printlines.java
    Source code

---
+++ Printlines.java
@@ -0,0 +1,10 @@
+// Showing the difference between println and print
+public class Printlines
+{
+  public static void main(String[] args)
+  {
+    System.out.println("println will advance the cursor to the next line after output");
+    System.out.print("However, print will ");
+    System.out.print("not advance to the next line after output.");
+  }
+}
